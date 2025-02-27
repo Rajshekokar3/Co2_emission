a603829638b64b8f19b8d1591d3ad82b4900c701
Raj Shekokar <rajk90794@gmail.com>
2025-02-27 11:31:07 +0530

    pushing

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,30 +1,21 @@
 # Data Preprocessing Logic
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import streamlit as st
-import joblib  
-from sklearn.preprocessing import StandardScaler
 
+import joblib 
 
 
 def load_models():
     """Load the pre-trained scaler and label encoders."""
-    scaler = joblib.load("../models/Scaler.pkl")  
-    encoders = joblib.load("../models/label_ecoder.pkl")  # ✅ Fixed filename
-    print("Loaded encoders:", encoders.keys())  #
-    return scaler, encoders
+    scaler = joblib.load("../models/scaler.pkl")
+    encoders = joblib.load("../models/label_ecoder.pkl")  # ✅ Fixed filename #
+    return scaler, encoders 
 
 # Load models once
 scaler, encoders = load_models()
 
+
 def data_preprocess(df):
-    """Preprocess data: Apply label encoding and scaling."""
-
-
-    # Display processed data
-    st.write("✅ Data Preprocessing Done!")
-    st.dataframe(pd.DataFrame(df_scaled, columns=df.columns))  
-
-    return df_scaled  # Return preprocessed data
+    for col in df.columns:
+        if col in encoders:  # Check if the column has an encoder
+            df[col] = df[col].apply(lambda x: encoders[col].transform([x])[0] if x in encoders[col].classes_ else -1)
+    input=scaler.transform(df)
+    return(input)
--- scripts/model_training.py
+++ scripts/model_training.py
@@ -1,1 +1,17 @@
 # Model Training Logic
+import joblib
+import streamlit as st
+import pickle
+
+@st.cache_resource
+def load_model():
+    with open("../scripts/best_model.pkl",'rb') as file:
+        model=pickle.load(file)
+    return model
+
+
+model=load_model()
+
+def model_traning(df):
+    prediction=model.predict(df)
+    return prediction
